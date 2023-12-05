84c549b568f0c698448a8ba611f98b689d45e7ad
Joshua Biong <143684369+JBiong@users.noreply.github.com>
2023-12-05 15:18:17 +0800

    Delete acadzen/acadzen/src directory

--- acadzen/acadzen/src/main/java/com/acadzen/acadzen/Repository/ImageRepository.java
+++
@@ -1,7 +0,0 @@
-// package com.acadzen.acadzen.Repository;
-
-// import org.springframework.data.jpa.repository.JpaRepository;
-// import com.acadzen.acadzen.Entity.ImageEntity;
-
-// public interface ImageRepository extends JpaRepository<ImageEntity, Integer> {
-// }
